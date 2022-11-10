516bec200fc059bb017625f161f25d68692d65f4
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-11-09 20:08:27 -0600

    Created Measure schema

---
+++ models/Measure.js
@@ -0,0 +1,37 @@
+const {Schema} = require('mongoose');
+
+const measureSchema = new Schema(
+    {
+        date: {
+            type: String,
+            required: true
+        },
+        weight: {
+            type: String,
+            required: true
+        },
+        bodyFatPercentage: {
+            type: String,
+            required: true
+        },
+        leanBodyWeight: {
+            type: String,
+            required: true
+        },
+        bodyFat: {
+            type: String,
+            required: true
+        },
+        bodyType: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
+    }
+);
+
+module.exports = measureSchema;
