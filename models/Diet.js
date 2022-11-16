a3316495fb8b34fc72fe83f95745ec06a619f45c
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-11-15 20:10:33 -0600

    Added unique option to filename in dietSchema

--- models/Diet.js
+++ models/Diet.js
@@ -8,7 +8,8 @@ const dietSchema = new Schema(
         },
         fileName: {
             type: String,
-            required: true
+            required: true,
+            unique: true
         }
     }
 );
