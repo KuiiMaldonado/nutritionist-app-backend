752167aa7fd5398f7c8c2c5b323b8d3efd265f0a
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-11-15 20:09:27 -0600

    Added userDiets in User model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const {Schema, model} = require('mongoose');
 const bcrypt = require('bcrypt');
 const measureSchema = require('./Measure');
+const dietSchema = require('./Diet');
 
 const userSchema = new Schema(
     {
@@ -38,6 +39,7 @@ const userSchema = new Schema(
             default: false
         },
         userMeasures: [measureSchema],
+        userDiets: [dietSchema],
     },
     {
         toJSON: {
