ae84e68cfdb1dbdf59876a61fd9c78a41fe7fb5a
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2023-01-03 20:01:34 -0600

    Updated validateCaptcha route

--- controllers/api/google-routes.js
+++ controllers/api/google-routes.js
@@ -7,8 +7,17 @@ router.post('/validateCaptcha', async (req, res) => {
         const response = await axios.post('https://www.google.com/recaptcha/api/siteverify', {
             secret: process.env.RECAPTCHA_SECRET_KEY,
             response: req.body.captchaToken
+        }, {
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            }
         });
-        console.log(response);
+        if (response.data.success) {
+            res.status(200);
+        }
+        else {
+            res.status(500).json({message: 'Failed to validate the captcha'});
+        }
     } catch (error) {
         console.error(error);
         res.status(500).json(error);
